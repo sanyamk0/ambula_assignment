7d8ca716721a24e2d58bcba46cc1998758b8e0ac
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-07-07 05:31:54 +0530

    Preventing TextArea Expansion

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -105,7 +105,7 @@ const ContactPage = () => {
                     onChange={(e) => setMessage(e.target.value)}
                     required
                     rows="4"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md resize-none h-44 border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none sm:text-sm sm:leading-6"
                   ></textarea>
                 </div>
               </div>
